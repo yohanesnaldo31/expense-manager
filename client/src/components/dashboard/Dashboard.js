19ca3c85e481f9f98219a6b3cfa571d4b22a3fca
Yohanes Naldo <52414241+yohanesnaldo31@users.noreply.github.com>
2019-07-08 22:09:17 +0700

    Update Dashboard.js

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -16,11 +16,7 @@ const Dashboard = (props) => {
             <div className="row">
                 <div className="col s12 center-align">
                     <h4>
-                    <b>Hey there,</b> {user.username.split(" ")[0]}
-                    <p className="flow-text grey-text text-darken-1">
-                        You are logged into a full-stack{" "}
-                        <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-                    </p>
+                    {user.username.split(" ")[0]}
                     </h4>
                     <button
                     style={{
